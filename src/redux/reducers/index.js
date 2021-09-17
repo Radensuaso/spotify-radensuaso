18d7a992acd3f322fb2523aefff2af9c9931b985
Radensuaso <andrels9283@gmail.com>
2021-09-17 16:03:31 +0100

    Update index.js

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from "redux";
 import mediaReducer from "./mediaReducer";
 import libraryReducer from "./libraryReducer";
-import { ADD_TO_LIBRARY, REMOVE_FROM_LIBRARY } from "../actions";
 
 const reducers = combineReducers({
   media: mediaReducer,
